be7d064a986e1c00e1376ada4b29f23804aecccd
GeraldoFerreira88 <lojacondolaroma@outlook.com>
2025-06-04 00:56:43 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>FelipeVerse</h1>
+      <p>Upload uma imagem e escreva um prompt para gerar um vídeo com IA.</p>
+    </div>
+  );
+}
